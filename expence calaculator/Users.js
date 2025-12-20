ad9ce4a75bbd9cb6ca400aa18375776ef6d3f4a7
himanshuhimmy <122083033+himanshuhimmy@users.noreply.github.com>
2025-12-20 12:16:12 +0530

    income expence

---
+++ expence calaculator/Users.js
@@ -0,0 +1,7 @@
+let users = [
+  {
+    himanshu: { id: 1, access: { username: "himmy", password: "himmy" } },
+    pratik: { id: 2, access: { username: "pratik", password: "pratik" } },
+    ramesh: { id: 3, access: { username: "ramesh", password: "ramesh" } },
+  },
+];
