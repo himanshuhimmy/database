36cbcd45ca40a274229e1507669dc3615c0c5aea
himanshuhimmy <122083033+himanshuhimmy@users.noreply.github.com>
2025-12-31 13:34:15 +0530

    aaa

--- expence calaculator/Users.js
+++ expence calaculator/Users.js
@@ -1,5 +1,5 @@
 let users = [
-  { id: 1, name: `himanshu`, access: { username: "himmy", password: "himmy" } },
-  { id: 2, name: `pratik`, access: { username: "pratik", password: "pratik" } },
-  { id: 3, name: `ramesh`, access: { username: "ramesh", password: "ramesh" } },
+  { id: 1, name: `himanshu`, username: "himmy", password: "himmy" },
+  { id: 2, name: `pratik`, username: "pratik", password: "pratik" },
+  { id: 3, name: `ramesh`, username: "ramesh", password: "ramesh" },
 ];
